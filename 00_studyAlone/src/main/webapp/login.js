97f66c71f13e01860db2d30945704352bf84b4bd
Jisu Kim <tlawl912@gmail.com>
2022-05-24 23:26:47 +0900

    commit day+39 I don't know what i'm doing right now,, I can't understand the machine  working. I think I write right words but it doesn't work. I made function but how come it dosen't show me the alert TT I gonna figure this out later

---
+++ 00_studyAlone/src/main/webapp/login.js
@@ -0,0 +1,40 @@
+function login(){
+	let id = document.login.id;
+	let pw = document.login.pw;
+	let pwCheck = document.login.pwCheck;
+	
+	if(mustInfo(id)){
+		alert('필수정보입니다');
+		id.focus;
+		return false;
+	}
+	
+	if(mustInfo(pw)){
+		alert('필수정보입니다');
+		pw.focus;
+		return false;
+	}
+	
+	if(lessThan(pw,8) || 
+		contains(pw,'qwertyuiopasdfghjklzxcvbnm')||
+		contains(pw,'QWERTYUIOPASDFGHJKLZXCVBNM')||
+		contains(pw,'1234567890')||
+		contains(pw,'!@#$%^&*')){
+		alert('비밀번호는 8~16 영대 소문자, 숫자 , 특수문자를 포함해주세요');
+		pw.focus;
+		return false;	
+	}
+	
+	if(mustInfo(pwCheck)){
+		alert('필수정보입니다');
+		pwCheck.focus;
+		return false;
+	}
+
+	if(check(pw,pwCheck)){
+		alert('비밀번호가 일치하지 않습니다');
+		pwCheck.focus;
+		return false;
+	}
+	return true;
+}

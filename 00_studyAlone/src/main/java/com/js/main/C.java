130608f4fd77978ac7d98f9804e70eb328ace3f4
Jisu Kim <tlawl912@gmail.com>
2022-05-25 20:43:57 +0900

    commit day+40 congratulation!!!! it's already passed 40 days. Today, I learn MVC(Model View Controller) = Model2. The process is complicated but it's worth to learn. 一度整理してみたら思ったよりはうまくできてよかったです！

---
+++ 00_studyAlone/src/main/java/com/js/main/C.java
@@ -0,0 +1,24 @@
+package com.js.main;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/C")
+public class C extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("fileStudy3.html").forward(request, response);
+	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		M.get(request);
+		request.getRequestDispatcher("fileStudy3_output.jsp").forward(request, response);
+		
+	}
+
+}
---
+++ 00_studyAlone/src/main/java/com/js/main/M.java
@@ -0,0 +1,14 @@
+package com.js.main;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class M {
+
+	public static void get(HttpServletRequest request) {
+	String id =	request.getParameter("id");
+	String pw =	request.getParameter("pw");
+	
+	
+	}
+
+}
